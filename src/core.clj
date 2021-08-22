a82481128107c974d3e55c6b2c3adc1df90f1294
heykieran <heykieran@gmail.com>
2021-08-22 13:25:28 -0400

    Minimal Cleanup

--- src/core.clj
+++ src/core.clj
@@ -2,7 +2,6 @@
   (:require
    [xlparse :as parse]
    [shunting :as sh]
-   [shunting-xl :as shxl]
    [ast-processing :as ast]
    [clojure.walk :as walk]
    [excel :as excel]
@@ -14,114 +13,8 @@
       (parse/nest-ast))
   (-> "=1+2+3"
       (parse/parse-to-tokens)
-      #_(parse/nest-ast))
-  (shxl/parse-simple-expression-tokens
-   [{:value "1", :type :Operand, :sub-type :Number}
-    {:value "+", :type :OperatorInfix, :sub-type :Math}
-    {:value "2", :type :Operand, :sub-type :Number}
-    {:value "+", :type :OperatorInfix, :sub-type :Math}
-    {:value "3", :type :Operand, :sub-type :Number}])
-
-  (shxl/parse-simple-expression-tokens
-   [{:value "10", :type :Operand, :sub-type :Number}
-    {:value "+", :type :OperatorInfix, :sub-type :Math}
-    {:value "20", :type :Operand, :sub-type :Number}
-    {:value "+", :type :OperatorInfix, :sub-type :Math}
-    {:value "30", :type :Operand, :sub-type :Number}])
-
-  (shxl/parse-expression-tokens
-   [[{:value "1", :type :Operand, :sub-type :Number}
-     {:value "+", :type :OperatorInfix, :sub-type :Math}
-     {:value "2", :type :Operand, :sub-type :Number}
-     {:value "+", :type :OperatorInfix, :sub-type :Math}
-     {:value "3", :type :Operand, :sub-type :Number}]
-    {:value "*", :type :OperatorInfix, :sub-type :Math}
-    [{:value "10", :type :Operand, :sub-type :Number}
-     {:value "+", :type :OperatorInfix, :sub-type :Math}
-     {:value "20", :type :Operand, :sub-type :Number}
-     {:value "+", :type :OperatorInfix, :sub-type :Math}
-     {:value "30", :type :Operand, :sub-type :Number}]])
+      (parse/nest-ast))
   :end)
-
-(def ast-raw
-  (-> "=(1+2+3)*(10+20+30)/(40+50+60)"
-      (parse/parse-to-tokens)
-      (parse/nest-ast)))
-
-(def ast
-  {0
-   {:value ""
-    :type :Subexpression
-    :sub-type :Start
-    1
-    {:value ""
-     :type :Subexpression
-     :sub-type :Start
-     2 {:value "1", :type :Operand, :sub-type :Number}
-     3 {:value "+", :type :OperatorInfix, :sub-type :Math}
-     4 {:value "2", :type :Operand, :sub-type :Number}
-     5 {:value "+", :type :OperatorInfix, :sub-type :Math}
-     6 {:value "3", :type :Operand, :sub-type :Number}}
-    8 {:value "*", :type :OperatorInfix, :sub-type :Math}
-    9
-    {:value ""
-     :type :Subexpression
-     :sub-type :Start
-     10 {:value "10", :type :Operand, :sub-type :Number}
-     11 {:value "+", :type :OperatorInfix, :sub-type :Math}
-     12 {:value "20", :type :Operand, :sub-type :Number}
-     13 {:value "+", :type :OperatorInfix, :sub-type :Math}
-     14 {:value "30", :type :Operand, :sub-type :Number}}
-    16 {:value "/", :type :OperatorInfix, :sub-type :Math}
-    17
-    {:value ""
-     :type :Subexpression
-     :sub-type :Start
-     18 {:value "40", :type :Operand, :sub-type :Number}
-     19 {:value "+", :type :OperatorInfix, :sub-type :Math}
-     20 {:value "50", :type :Operand, :sub-type :Number}
-     21 {:value "+", :type :OperatorInfix, :sub-type :Math}
-     22 {:value "60", :type :Operand, :sub-type :Number}}}})
-
-(defn should-simplify? [form]
-  (and
-   (map? form)
-   (identical?
-    :Subexpression
-    (:type form))))
-
-(defn expressions->vector [exps]
-  (if (map? exps)
-    (vector exps)
-    (into [] exps)))
-
-(defn add-expression [form & [debug]]
-  (assoc
-   form
-   :clj
-   (expressions->vector
-    (shxl/parse-simple-expression-tokens
-     (keep
-      (fn [[k v]]
-        (when
-         (number? k)
-          v))
-      form)
-     debug))))
-
-(defn filter-expression [form]
-  (apply dissoc form
-         (filter #(not= % :clj) (keys form))))
-
-(defn as-code [ast & {:keys [debug]}]
-  (walk/postwalk
-   (fn [form]
-     (if (should-simplify? form)
-       (-> form
-           (add-expression debug)
-           (filter-expression))
-       form))
-   ast))
 
 (defn validate [r-map]
   (println "Check" (:value r-map) "=" (:result r-map) "for" (:formula r-map))
@@ -158,48 +51,11 @@
 
 
 (comment
-  (as-code
-   (-> "=(1+2+3)*(10+20+30)/(40+50+60)"
-       (parse/parse-to-tokens)
-       (parse/nest-ast)
-       (parse/wrap-ast)))
-
-  (as-code
-   (-> "=(1+2+3)+4"
-       (parse/parse-to-tokens)
-       (parse/nest-ast)
-       (parse/wrap-ast)))
-
-  (as-code
-   (-> "=(1+2+3)"
-       (parse/parse-to-tokens)
-       (parse/nest-ast)
-       (parse/wrap-ast)))
-
-  (as-code
-   (-> "=max(1,max(1,(2),4))"
-       (parse/parse-to-tokens)
-       (parse/nest-ast)
-       (parse/wrap-ast))
-   :debug false)
-
-  (as-code
-   (-> "=1+max(1,2)"
-       (parse/parse-to-tokens)
-       (parse/nest-ast)
-       (parse/wrap-ast))
-   :debug false)
 
   (-> "=max(1,max(1,(2),4))"
       (parse/parse-to-tokens)
       (parse/nest-ast)
       (parse/wrap-ast))
-
-  (as-code
-   (-> "=max(1,max(1,(2),4))"
-       (parse/parse-to-tokens)
-       (parse/nest-ast)
-       (parse/wrap-ast)))
 
   (-> "=max(1,max(1,(2+3),4))"
       (parse/parse-to-tokens)
@@ -334,7 +190,7 @@
        (filter #(false? (:ok? %))))
 
   (run-tests)
-  
+
   (boolean 1.0)
   (not (boolean 0))
   (functions/fn-not (if (not= 1.0 1.0) 1.0 0.0))
